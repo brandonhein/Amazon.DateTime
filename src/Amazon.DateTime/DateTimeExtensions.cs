b2ca1754ad9a866cbe732acedd01e487d0e1ee6b
Brandon Hein <bhein@lssu.edu>
2020-11-01 10:10:24 -0500

    allow clients of the package to provide observes dst for time calc
    Places in Arizona, don't observe daylight savings

--- src/Amazon.DateTime/DateTimeExtensions.cs
+++ src/Amazon.DateTime/DateTimeExtensions.cs
@@ -8,66 +8,66 @@
         /// Converts a UTC <see cref="DateTime"/> to a eastern time string format
         /// <para>FORMAT: yyyy-MM-ddTHH:mm:ss.fffzzz</para>
         /// </summary>
-        public static string ToEasternString(this DateTime dateTime)
+        public static string ToEasternString(this DateTime dateTime, bool observesDaylightSavings = true)
         {
             var easternDate = dateTime.ToEastern();
             return string.Concat(easternDate.ToString(Format.StandardDateTime),
-                easternDate.IsInDaylightSavingsTime() ? EasternDateTime.DaylightOffset : EasternDateTime.StandardOffset);
+                observesDaylightSavings && easternDate.IsInDaylightSavingsTime() ? EasternDateTime.DaylightOffset : EasternDateTime.StandardOffset);
         }
 
         /// <summary>
         /// Converts a UTC <see cref="DateTime"/> to a central time string format
         /// <para>FORMAT: yyyy-MM-ddTHH:mm:ss.fffzzz</para>
         /// </summary>
-        public static string ToCentralString(this DateTime dateTime)
+        public static string ToCentralString(this DateTime dateTime, bool observesDaylightSavings = true)
         {
             var centralDate = dateTime.ToCentral();
             return string.Concat(centralDate.ToString(Format.StandardDateTime),
-                centralDate.IsInDaylightSavingsTime() ? CentralDateTime.DaylightOffset : CentralDateTime.StandardOffset);
+                observesDaylightSavings && centralDate.IsInDaylightSavingsTime() ? CentralDateTime.DaylightOffset : CentralDateTime.StandardOffset);
         }
 
         /// <summary>
         /// Converts a UTC <see cref="DateTime"/> to a mountain time string format
         /// <para>FORMAT: yyyy-MM-ddTHH:mm:ss.fffzzz</para>
         /// </summary>
-        public static string ToMountainString(this DateTime dateTime)
+        public static string ToMountainString(this DateTime dateTime, bool observesDaylightSavings = true)
         {
             var mountainDate = dateTime.ToMountain();
             return string.Concat(mountainDate.ToString(Format.StandardDateTime),
-                mountainDate.IsInDaylightSavingsTime() ? MountainDateTime.DaylightOffset : MountainDateTime.StandardOffset);
+                observesDaylightSavings && mountainDate.IsInDaylightSavingsTime() ? MountainDateTime.DaylightOffset : MountainDateTime.StandardOffset);
         }
 
         /// <summary>
         /// Converts a UTC <see cref="DateTime"/> to a pacific time string format
         /// <para>FORMAT: yyyy-MM-ddTHH:mm:ss.fffzzz</para>
         /// </summary>
-        public static string ToPacificString(this DateTime dateTime)
+        public static string ToPacificString(this DateTime dateTime, bool observesDaylightSavings = true)
         {
             var pacificDate = dateTime.ToPacific();
             return string.Concat(pacificDate.ToString(Format.StandardDateTime),
-                pacificDate.IsInDaylightSavingsTime() ? PacificDateTime.DaylightOffset : PacificDateTime.StandardOffset);
+                observesDaylightSavings && pacificDate.IsInDaylightSavingsTime() ? PacificDateTime.DaylightOffset : PacificDateTime.StandardOffset);
         }
 
         /// <summary>
         /// Converts a UTC <see cref="DateTime"/> to a alaska time string format
         /// <para>FORMAT: yyyy-MM-ddTHH:mm:ss.fffzzz</para>
         /// </summary>
-        public static string ToAlaskaString(this DateTime dateTime)
+        public static string ToAlaskaString(this DateTime dateTime, bool observesDaylightSavings = true)
         {
             var alaskaDate = dateTime.ToAlaska();
             return string.Concat(alaskaDate.ToString(Format.StandardDateTime),
-                alaskaDate.IsInDaylightSavingsTime() ? AlaskaDateTime.DaylightOffset : AlaskaDateTime.StandardOffset);
+                observesDaylightSavings && alaskaDate.IsInDaylightSavingsTime() ? AlaskaDateTime.DaylightOffset : AlaskaDateTime.StandardOffset);
         }
 
         /// <summary>
         /// Converts a UTC <see cref="DateTime"/> to a hawaii time string format
         /// <para>FORMAT: yyyy-MM-ddTHH:mm:ss.fffzzz</para>
         /// </summary>
-        public static string ToHawaiiString(this DateTime dateTime)
+        public static string ToHawaiiString(this DateTime dateTime, bool observesDaylightSavings = false)
         {
             var hawaiiDate = dateTime.ToHawaii();
             return string.Concat(hawaiiDate.ToString(Format.StandardDateTime),
-                hawaiiDate.IsInDaylightSavingsTime() ? HawaiiDateTime.DaylightOffset : HawaiiDateTime.StandardOffset);
+                observesDaylightSavings && hawaiiDate.IsInDaylightSavingsTime() ? HawaiiDateTime.DaylightOffset : HawaiiDateTime.StandardOffset);
         }
     }
 
