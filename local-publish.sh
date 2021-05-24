5e02021c6dc2ca785a00046fc73824572138ee6b
Brandon Hein <brandonhein19@gmail.com>
2021-05-23 20:11:37 -0400

    2.3.0 version

--- local-publish.sh
+++ local-publish.sh
@@ -1,6 +1,7 @@
 powershell Remove-Item './.publish' -Recurse
 
 powershell Remove-Item './src/Amazon.DateTime/bin' -Recurse
+dotnet clean ./src/Amazon.DateTime/Amazon.DateTime.csproj
 dotnet publish ./src/Amazon.DateTime/Amazon.DateTime.csproj -o ./.publish/Amazon.DateTime
 powershell Compress-Archive -Path './.publish/Amazon.DateTime/*' -DestinationPath './.publish/Amazon.DateTime.zip'
 powershell Remove-Item './.publish/Amazon.DateTime' -Recurse
